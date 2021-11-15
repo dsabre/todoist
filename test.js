9361a81b8bcd157e7a77e388d5eebd8d20dcdc1c
Daniele Sabre <daniele.sabre@jakala.com>
2021-11-15 20:32:27 +0100

    Develop

--- test.js
+++ test.js
@@ -1,7 +1,6 @@
-const Todoist = require("./index");
-require('dotenv').config();
-
-const todoist = new Todoist(process.env.TODOIST_API_TOKEN);
+const Todoist  = require("./index");
+const apiToken = require('fs').readFileSync('.env').toString().trim();
+const todoist  = new Todoist(apiToken);
 
 todoist.tasks.all(2278494454, 69081790, 2158879201, null, null, [5332546105, 5332546004]).then(p => {
     console.log(p);
