20afa4945c1dafe2fc440b9adaed3dde70f484ac
Daniele Sabre <daniele.sabre@gmail.com>
2022-01-11 14:55:43 +0100

    Develop

--- test.js
+++ test.js
@@ -2,6 +2,4 @@ const Todoist  = require("./index");
 const apiToken = require('fs').readFileSync('.env').toString().trim();
 const todoist  = new Todoist(apiToken);
 
-todoist.tasks.all(2278494454, 69081790, 2158879201, null, null, [5332546105, 5332546004]).then(p => {
-    console.log(p);
-});
+todoist.projects.all().then(r => console.log(r));
